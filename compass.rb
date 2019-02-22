0b086f735d7840e3d8ed61323c8b017c827dbd6e
James Healy <james@yob.id.au>
2019-02-23 02:00:08 +1100

    initial commit
    * a very basic API client for Compass

---
+++ compass.rb
@@ -0,0 +1,121 @@
+require 'net/http'
+require 'json'
+
+class CompassClient
+  USER_AGENT = "iOS/12_1_2 type/iPhone CompassEducation/4.5.3"
+  PATH_AUTH = "/services/admin.svc/AuthenticateUserCredentials"
+  PATH_NEWSFEED = "/services/mobile.svc/GetNewsFeed?sessionstate=readonly"
+  PATH_GET_MESSAGES = "/services/mobile.svc/GetMessages?sessionstate=readonly"
+  PATH_PST_CYCLES = "/services/mobile.svc/GetPstCycles?sessionstate=readonly"
+  PATH_CHECK_PARENT_DETAILS = "/services/mobile.svc/CheckParentDetails?sessionstate=readonly"
+  PATH_GET_PERSONAL_DETAILS = "/services/mobile.svc/GetPersonalDetails?sessionstate=readonly"
+  PATH_DOWNLOAD_FILE = "/services/FileDownload/FileRequestHandler" #FileDownloadType=1&file=215
+  
+  def initialize(hostname, username, password)
+    @hostname = hostname
+    @cookie = generate_cookie(username, password)
+  end
+
+  def check_parent_details
+    response = post(
+      path: PATH_CHECK_PARENT_DETAILS,
+      headers: {"Cookie" => @cookie},
+    )
+    JSON.parse(response.body)
+  end
+
+  def download_file(file_id: )
+    response = get(
+      path: "#{PATH_DOWNLOAD_FILE}?FileDownloadType=1&file=#{file_id}",
+      headers: {"Cookie" => @cookie},
+    )
+    puts response.body
+  end
+
+  def get_messages
+    response = post(
+      path: PATH_GET_MESSAGES,
+      headers: {"Cookie" => @cookie},
+    )
+    JSON.parse(response.body)
+  end
+
+  def get_news_feed
+    response = post(
+      path: PATH_NEWSFEED,
+      headers: {"Cookie" => @cookie},
+    )
+    JSON.parse(response.body)
+  end
+
+  def get_personal_details
+    response = post(
+      path: PATH_GET_PERSONAL_DETAILS,
+      headers: {"Cookie" => @cookie},
+    )
+    JSON.parse(response.body)
+  end
+
+  # this seems to be parent teacher interview slots
+  def get_pst_cycles
+    response = post(
+      path: PATH_PST_CYCLES,
+      headers: {"Cookie" => @cookie},
+    )
+    JSON.parse(response.body)
+  end
+
+  private
+
+  def generate_cookie(username, password)
+    response = post(
+      path: PATH_AUTH,
+      headers: {"Content-Type" => "application/json"},
+      body: JSON.dump({username: username, password: password, sessionstate: "readonly"})
+    )
+    session_id = response["Set-Cookie"][/ASP.NET_SessionId=[a-f0-9\-]+/]
+    raise "login failed" if session_id.nil?
+    session_id
+  end
+
+  def get(path:, headers: {})
+    headers["User-Agent"] = USER_AGENT
+
+    request = Net::HTTP::Get.new(path, headers)
+
+    response = Net::HTTP.start(@hostname, 443, use_ssl: true) do |http|
+      http.request(request)
+    end
+
+    if response.code.to_i == 200
+      response
+    else
+      raise "login failed"
+    end
+  end
+
+  def post(path:, headers: {}, body: nil )
+    headers["User-Agent"] = USER_AGENT
+
+    request = Net::HTTP::Post.new(path, headers)
+    request.body = body if body
+
+    response = Net::HTTP.start(@hostname, 443, use_ssl: true) do |http|
+      http.request(request)
+    end
+
+    if response.code.to_i == 200
+      response
+    else
+      raise "login failed"
+    end
+  end
+end
+
+hostname, username, password = *ARGV
+if hostname.nil? || username.nil? || password.nil?
+  $stderr.puts "USAGE: ruby compass.rb <school hostname> <username> <password>"
+  exit(1)
+end
+client = CompassClient.new(hostname, username, password)
+puts client.get_news_feed
