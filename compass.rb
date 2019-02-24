32e270de611001b2129991ffd19271242b89f74d
James Healy <james@yob.id.au>
2019-02-24 23:45:39 +1100

    Start working on some value objects

--- compass.rb
+++ compass.rb
@@ -1,6 +1,62 @@
 require 'net/http'
 require 'json'
 
+class CompassJSON
+  def self.parse(data)
+    if data.is_a?(String) || data.is_a?(Integer)
+      data
+    elsif data.is_a?(Array)
+      data.map { |item| CompassJSON.parse(item) }
+    elsif data.is_a?(Hash)
+      if data.size == 1 && data.key?("d") # root of a response
+        data.fetch("d")
+      elsif data.fetch("__type","").starts_with?("GenericMobileResponse:")
+        GenericMobileResponse.new(data.fetch("data"))
+      elsif data.fetch("__type","").starts_with?("NewsItem:")
+        NewsItem.new(data.fetch("data"))
+      else
+        CompassJSON.parse(data)
+      end
+    else
+      raise "Not sure what to do with: #{data.inspect}"
+    end
+  end
+end
+
+class GenericMobileResponse
+  attr_reader :data
+
+  def initialize(data)
+    @data = data
+  end
+
+  def eql?(other)
+    @data.hash == other.data.hash
+  end
+end
+
+class NewsItem
+  attr_reader :attachments, :content, :content_html, :id, :post_date, :post_date_utc
+  attr_reader :priority, :title, :uploader, :user_image_url
+
+  def initialize(data)
+    @id = data.fetch("newsItemId", 0)
+    @attachments = data.fetch("attachments", [])
+    @content = data.fetch("content", "")
+    @content_html = data.fetch("contentHtml", "")
+    @post_date = data.fetch("", "postDate")
+    @post_date_utc = data.fetch("postDateUtc", "")
+    @priority = data.fetch("priority", false)
+    @title = data.fetch("title", "")
+    @uploader = data.fetch("uploader", "")
+    @user_image_url = data.fetch("userImageUrl", "")
+  end
+
+  def eql?(other)
+    other.is_a?(NewsItem) && @id == other.id
+  end
+end
+
 class CompassClient
   USER_AGENT = "iOS/12_1_2 type/iPhone CompassEducation/4.5.3"
   PATH_AUTH = "/services/admin.svc/AuthenticateUserCredentials"
@@ -9,8 +65,8 @@ class CompassClient
   PATH_PST_CYCLES = "/services/mobile.svc/GetPstCycles?sessionstate=readonly"
   PATH_CHECK_PARENT_DETAILS = "/services/mobile.svc/CheckParentDetails?sessionstate=readonly"
   PATH_GET_PERSONAL_DETAILS = "/services/mobile.svc/GetPersonalDetails?sessionstate=readonly"
-  PATH_DOWNLOAD_FILE = "/services/FileDownload/FileRequestHandler" #FileDownloadType=1&file=215
-  
+  PATH_DOWNLOAD_FILE = "/services/FileDownload/FileRequestHandler"
+
   def initialize(hostname, username, password)
     @hostname = hostname
     @cookie = generate_cookie(username, password)
@@ -21,7 +77,7 @@ class CompassClient
       path: PATH_CHECK_PARENT_DETAILS,
       headers: {"Cookie" => @cookie},
     )
-    JSON.parse(response.body)
+    CompassJSON.parse(JSON.parse(response.body))
   end
 
   def download_file(file_id: )
@@ -37,7 +93,7 @@ class CompassClient
       path: PATH_GET_MESSAGES,
       headers: {"Cookie" => @cookie},
     )
-    JSON.parse(response.body)
+    CompassJSON.parse(JSON.parse(response.body))
   end
 
   def get_news_feed
@@ -45,7 +101,7 @@ class CompassClient
       path: PATH_NEWSFEED,
       headers: {"Cookie" => @cookie},
     )
-    JSON.parse(response.body)
+    CompassJSON.parse(JSON.parse(response.body))
   end
 
   def get_personal_details
@@ -53,7 +109,7 @@ class CompassClient
       path: PATH_GET_PERSONAL_DETAILS,
       headers: {"Cookie" => @cookie},
     )
-    JSON.parse(response.body)
+    CompassJSON.parse(JSON.parse(response.body))
   end
 
   # this seems to be parent teacher interview slots
@@ -62,7 +118,7 @@ class CompassClient
       path: PATH_PST_CYCLES,
       headers: {"Cookie" => @cookie},
     )
-    JSON.parse(response.body)
+    CompassJSON.parse(JSON.parse(response.body))
   end
 
   private
@@ -112,10 +168,12 @@ class CompassClient
   end
 end
 
-hostname, username, password = *ARGV
-if hostname.nil? || username.nil? || password.nil?
-  $stderr.puts "USAGE: ruby compass.rb <school hostname> <username> <password>"
-  exit(1)
+if __FILE__ == $0
+  hostname, username, password = *ARGV
+  if hostname.nil? || username.nil? || password.nil?
+    $stderr.puts "USAGE: ruby compass.rb <school hostname> <username> <password>"
+    exit(1)
+  end
+  client = CompassClient.new(hostname, username, password)
+  puts client.get_news_feed.inspect
 end
-client = CompassClient.new(hostname, username, password)
-puts client.get_news_feed
